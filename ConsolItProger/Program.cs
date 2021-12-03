cfdc3ff0fdcd186d20b61bf7473f81842c0d05d7
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2021-12-03 17:36:52 +0200

    Add project files.

---
+++ ConsolItProger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace consolItProger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the area of a circle");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine("Area of a circle with radius {0} is equal to {1}", radius, area);
+        }
+    }
+}
